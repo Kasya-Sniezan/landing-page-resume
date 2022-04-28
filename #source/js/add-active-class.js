6fb4d386152f08e627315aa6d9b54b6efbe807cc
Kasya Sniezan <kasya.sniezan@gmail.com>
2022-04-28 13:18:11 +0300

    update

--- #source/js/add-active-class.js
+++ #source/js/add-active-class.js
@@ -1,14 +1,12 @@
-/*Добавление класса active для меню в мобилках*/
+/*Добавление класса active для меню*/
 const iconMenu = document.querySelector('.header__icon');
 const menuBody = document.querySelector('.header__body');
 const menuList = document.querySelector('.menu__list');
-const langRow = document.querySelector('.lang__row');
-if (iconMenu){
+if (iconMenu) {
     iconMenu.addEventListener("click", function (e) {
         document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
         menuList.classList.toggle('_active');
-        langRow.classList.toggle('_active');
     });
 }
